84afcf3d96090747e8e6ac57f46744382a66a347
Jorge Ramón <jorge.ramon2128@gmail.com>
2024-03-04 19:40:46 -0600

    Removed fastify, added Express

--- src/index.js
+++ src/index.js
@@ -1,64 +1,44 @@
 require("dotenv").config();
 
-const fastify = require("fastify");
-const fastifyIO = require("fastify-socket.io");
+const { Server } = require("socket.io");
+const express = require("express");
+const http = require("http");
 const { join } = require("path");
 const logger = require("./desktop/logger");
-const fastifyCors = require("@fastify/cors");
 
-const server = fastify();
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
-server
-  .register(fastifyCors, {
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  })
-  .register(fastifyIO, {
-    cors: {
-      origin: "*",
-      methods: ["GET", "POST", "PUT", "DELETE"],
-    },
-  })
-  .register(require("@fastify/view"), {
-    engine: {
-      pug: require("pug"),
-    },
-    root: join(__dirname, "templates"),
-  })
-  .register(require("@fastify/static"), {
-    root: join(__dirname, "assets"),
-    prefix: "/assets/",
-  });
+app.use("/assets", express.static(join(__dirname, "./assets")));
+app.set("views", join(__dirname, "./templates"));
+app.set("view engine", "pug");
 
-server.get("/", (_, reply) => reply.view("index.pug"));
+app.get("/", (_, response) => response.render("index.pug"));
 
-server.ready().then(() => {
-  logger.debug("Listening port: " + (process.env.SERVER_PORT || 2000));
+io.engine.on("connection_error", (err) => {
+  logger.error("Connection error");
+  logger.error(JSON.stringify(err));
+});
 
-  server.io.engine.on("connection_error", (err) => {
-    logger.error("Connection error");
-    logger.error(JSON.stringify(err));
-  });
+io.of("/client").on("connect", (socket) => {
+  logger.info("Client connected...");
 
-  server.io.of("/client").on("connect", (socket) => {
-    logger.info("Client connected...");
-
-    socket.on("current-song:server", () => {
-      logger.debug("[Client] Requesting current song...");
-      server.io.of("/desktop").emit("current-song:desktop");
-    });
-  });
-
-  server.io.of("/desktop").on("connect", (socket) => {
-    logger.info("Desktop connected...");
-
-    socket.on("song:server", (song) => {
-      logger.debug("[Desktop] Sending current song: " + JSON.stringify(song));
-      server.io.of("/client").emit("song:client", song);
-    });
+  socket.on("current-song:server", () => {
+    logger.debug("[Client] Requesting current song...");
+    io.of("/desktop").emit("current-song:desktop");
   });
 });
 
-server.listen({
-  port: process.env.SERVER_PORT || 2000,
+io.of("/desktop").on("connect", (socket) => {
+  logger.info("Desktop connected...");
+
+  socket.on("song:server", (song) => {
+    logger.debug("[Desktop] Sending current song: " + JSON.stringify(song));
+    io.of("/client").emit("song:client", song);
+  });
+});
+
+server.listen(process.env.SERVER_PORT || 2000, () => {
+  logger.debug("Listening port: " + (process.env.SERVER_PORT || 2000));
 });
